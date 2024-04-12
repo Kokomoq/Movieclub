1408bf96c0fed211a88ef9f351dcdda388bd557e
Kokomoq <140902777+Kokomoq@users.noreply.github.com>
2024-04-12 06:09:21 +0200

    Add files via upload

---
+++ src/main/java/com/example/movieclub/domain/movie/MovieDtoMapper.java
@@ -0,0 +1,28 @@
+package com.example.movieclub.domain.movie;
+
+import com.example.movieclub.domain.movie.Movie;
+import com.example.movieclub.domain.movie.dto.MovieDto;
+import com.example.movieclub.domain.rating.Rating;
+
+class MovieDtoMapper {
+    static MovieDto map(Movie movie) {
+        double avgRating = movie.getRatings().stream()
+                .map(Rating::getRating)
+                .mapToDouble(val -> val)
+                .average().orElse(0);
+        int ratingCount = movie.getRatings().size();
+        return new MovieDto(
+                movie.getId(),
+                movie.getTitle(),
+                movie.getOriginalTitle(),
+                movie.getShortDescription(),
+                movie.getDescription(),
+                movie.getYoutubeTrailerId(),
+                movie.getReleaseYear(),
+                movie.getGenre().getName(),
+                movie.isPromoted(),
+                movie.getPoster(),
+                avgRating,
+                ratingCount);
+    }
+}
